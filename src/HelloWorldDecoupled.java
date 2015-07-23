015b75999cca732f6f4ba6ba672c8678bb85c945
Vitaliy <GarazdVZ@gmail.com>
2015-07-23 21:10:57 +0300

    Delete HelloWorldDecoupled

--- src/HelloWorldDecoupled.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorldDecoupled {
-    public static void main(String[] args) {
-        MessageRenderer mr = new StandardOutMessageRenderer();
-        MessageProvider mp = new HelloWorldMessageProvider();
-        mr.setMessageProvider(mp);
-        mr.render();
-    }
-}
