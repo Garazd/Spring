5b77fe57273b0f2fbf88220a446b4752423ea9f1
Vitaliy <GarazdVZ@gmail.com>
2015-08-01 10:18:48 +0300

    Add HelloWorldSpringDI

---
+++ src/HelloWorldSpringDI.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloWorldSpringDI {
+    public static void main(String[] args) {
+// Инициализация ApplicationContext.
+        ApplicationContext ctx = new ClassPathXmlApplicationContext
+                ("META-INF/spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+    }
+}
