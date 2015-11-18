5d7eca30177a9d9e4aea7edf535470fe04c2b8f8
Garazd <GarazdVZ@gmail.com>
2015-11-18 15:13:33 +0200

    Added GreetingServiceImpl

---
+++ src/main/java/lessons/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package main.java.lessons.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Greeting, user!";
+    }
+}
